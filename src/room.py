d2c20625b9dc41d45bfe24ab0b654225f76f4deb
Ryan Hopkins <61099855+ryandotfi@users.noreply.github.com>
2020-02-15 23:03:00 -0500

    Update 2020.02.15-23:02:59

--- src/room.py
+++ src/room.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import requests
 
 class Building:
 
@@ -6,6 +7,17 @@ class Building:
         self.building = building
         self.room = room
         print("hi")
+        self.buildingName = ""
+        self.address = ""
+        self.city = ""
+        self.state = ""
+        self.zip = 0
+        
+        idJson = requests.get("https://maps.umd.edu/arcgis/rest/services/Layers/CampusMapDefault/MapServer/0/query?f=json&where=UPPER(NAME)%20like%20%27%25" + building + "%25%27%20OR%20BUILDINGID%20like%20%27%25" + building + "%25%27%20OR%20UPPER(BLDG_CODE)%20like%20%27%25ESJ%25%27&outFields=BUILDINGID").json()
+        if page.status_code != 200:
+            raise ValueError("Unknown Error - Website Out of Reach")
+        print(idJson)
+        
 
     def __str__(self):
         return "Building Code: {0}\nRoom Number: {1}".format(self.building, self.room)
