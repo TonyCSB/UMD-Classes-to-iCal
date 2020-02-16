5a673a4299c942b38044a6de6071afe7c01ca147
Tony Chen <tonychen@umd.edu>
2020-02-16 09:49:15 -0500

    Update 2020.02.16-09:49:15

--- src/iCal.py
+++ src/iCal.py
@@ -14,10 +14,14 @@ def generateCal(courseList):
     for course in courseList:
         for section in course.sectionList:
             e = Event()
-            e['summary']  = course.courseId
-            # TODO: finish date and time processing
-            e['dtstart'] = "20200216T" + section.start.strftime("%H%M%S")
-            e['dtend'] = "20200216T" + section.end.strftime("%H%M%S")
+            e.add('summary', course.courseId)
+
+            startTime = datetime.datetime.combine(datetime.date(2020, 2, 16), section.start)
+            e.add('dtstart', startTime)
+
+            endTime = datetime.datetime.combine(datetime.date(2020,2,16), section.end)
+            e.add('dtend', endTime)
+
             # TODO: [exdate] for breaks
             # TODO: finish repeat rules
 
@@ -25,8 +29,12 @@ def generateCal(courseList):
             for day in section.day:
                 days.append(day.value)
 
-            e['rrule'] = "FREQ=WEEKLY;WKST=SU;UNTIL=20200513T035959Z;BYDAY=" + ",".join(days)
-            e['location'] = section.room.getAddress()
+            e.add('rrule', {'freq': 'weekly',
+                            'wkst': 'su',
+                            'until': datetime.datetime(2020,5,20,0,0,0),
+                            'byday': days})
+
+            e.add('location', section.room.getAddress())
 
             des = section.room.building + " " + section.room.room
             des += "\nSection: " + course.sectionId
@@ -37,18 +45,6 @@ def generateCal(courseList):
     
     with open('test.ics', 'wb') as ics_file:
         ics_file.write(cal.to_ical())
-
-    # Read in the file
-    with open('test.ics', 'r') as file :
-        filedata = file.read()
-
-    # Replace the target string
-    # TODO: replace '\,' '\;' but not '\n'
-    filedata = filedata.replace("\\", "")
-
-    # Write the file out again
-    with open('test.ics', 'w') as file:
-        file.write(filedata)
 
 def main():
     c = Course("CMSC132", "0101", "202001")
