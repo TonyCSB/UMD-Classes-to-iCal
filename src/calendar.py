ba0a1fd10715a4551646ed24c63f5b7627c0e4ba
Tony Chen <tonychen@umd.edu>
2020-02-15 23:49:45 -0500

    Update 2020.02.15-23:49:45

--- src/calendar.py
+++ src/calendar.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
 from ics import Calendar, Event
+from course import Course
+from section import Section
 import datetime
 
 def generateCal(courseList):
     c = Calendar()
 
     for course in courseList:
+        for section in course.sectionList:
         e = Event()
-        
+        e.name = 
