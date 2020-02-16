76d241df4b953d46d8d8e33eb6e36e3dc80da797
Tony Chen <tonychen@umd.edu>
2020-02-15 23:45:06 -0500

    Update 2020.02.15-23:45:06

--- src/calendar.py
+++ src/calendar.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
 
 from ics import Calendar, Event
+import datetime
+
+def generateCal(courseList):
+    c = Calendar()
+
+    for course in courseList:
+        e = Event()
+        
