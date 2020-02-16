78f2006ffd245fdf95e0e436a040ec9deed89a8b
Tony Chen <tonychen@umd.edu>
2020-02-15 22:07:18 -0500

    Update 2020.02.15-22:07:18

--- src/course.py
+++ src/course.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
+import requests
+from bs4 import BeautifulSoup
+
 class Course:
 
-    # initializer
-    # to invoke, use Course(courseId, sectionId, termId)
+    # Initializer
+    # to create the object, use Course(courseId, sectionId, termId)
     def __init__(self, courseId, sectionId, termId):
+        print("hi")
