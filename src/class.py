d2bc180633e08ab181ab1548dd77c1e8a9e55bb6
Tony Chen <tonychen@umd.edu>
2020-02-15 22:50:54 -0500

    Update 2020.02.15-22:50:54

--- src/class.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-class Section:
-
-    # Initializer
-    # to create the object, use Section(day, start, end, room, isLecture)
-    def __init__(self, day:str, start, end, room, isLecture: bool):
-        print("hi")
---
+++ src/section.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+from room import Building
+
+class Section:
+
+    # Initializer
+    # to create the object, use Section(day, start, end, room, isLecture)
+    def __init__(self, day:str, start, end, room: Building, isLecture: bool):
+        self.day = day
+        self.start = start
+        self.end = end
+        self.room = room
+        self.isLecture = isLecture
+        print("hi")
+
+    def __str__(self):
+        return "Section Days: {0}\nClass Start Time: {1}\nClass End Time: {2}\nRoom: {3}\nLecture: {4}".format(self.day, self.start, self.end, self.room, self.isLecture)
+
+    def __repr__(self):
+        return str(self)
