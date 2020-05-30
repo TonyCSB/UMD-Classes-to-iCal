481912a573876898f8228d5d46c77b951c28896b
Tony Chen <tonychen@umd.edu>
2020-05-30 09:44:03 +0800

    Update 2020.05.30-09:44:03

--- src/user.py
+++ src/user.py
@@ -8,6 +8,9 @@ class User:
     self.password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     self.cookie = cookie
 
+  def checkPassword(self, pw: str):
+    return bcrypt.checkpw(pw.encode('utf-8'), self.password)
+    
   def __str__(self):
     return "Username: {0}\nHashed Passsword: {1}\nCookie Exists: {2}".format(self.username, self.password, bool(self.cookie))
 
