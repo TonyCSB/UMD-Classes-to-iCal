3567c06a881ae184de5b37be7f25f6b9c5e75209
Tony Chen <tonychen@umd.edu>
2020-05-30 09:41:22 +0800

    Update 2020.05.30-09:41:22

--- src/cookie.py
+++ src/cookie.py
@@ -2,7 +2,6 @@
 import pickle, requests
 
 # This module is intend to save and load cookie after login
-
 def saveCookie(session:requests.session):
     with open('.cookie', 'wb') as f:
         pickle.dump(session.cookies, f)
--- src/user.py
+++ src/user.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+import bcrypt
+
+class User:
+
+  def __init__(self, username: str, password: str, cookie = None):
+    self.username = username
+    self.password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    self.cookie = cookie
+
+  def __str__(self):
+    return "Username: {0}\nHashed Passsword: {1}\nCookie Exists: {2}".format(self.username, self.password, bool(self.cookie))
+
+  def __repr__(self):
+    return str(self)
